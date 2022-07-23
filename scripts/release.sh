7750710a486aecc238fcf71212e3f77545c764e9
Jahred <jahredhope@gmail.com>
2022-07-23 22:32:12 +1000

    Remove test versions

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,36 +5,16 @@ set -e
 
 version="v$(cat package.json | jq --raw-output ".version")"
 
-echo "Checking for existing tag"
 existing_tag=$(git ls-remote --tags -q origin "$version")
 
 if [[ -n "$existing_tag" ]]; then
-  echo "Warning: Tag for this version already exists. Exiting without action. $version"
+  echo "Warning: Tag for this version already exists. Exiting without action. Version: $version"
   exit 0
-fi
-
-changed_files=$(git status --porcelain)
-
-if [[ -n "$changed_files" ]]; then
-  echo "Error: Unable to release with pending changes"
-  echo "$changed_files"
-  exit 1
-fi
-
-pnpm build
-
-current_branch=$(git branch --show-current)
-
-if [[ $current_branch != "master" ]]; then
-  echo "Releasing from non-master branch is not supported"
-  exit 1
 fi
 
 branch_name=$(echo "$version" | cut -d "." -f 1)
 
 git checkout --detach
-
-echo "Committing new content"
 
 git add --force dist
 
@@ -42,4 +22,4 @@ git commit --message "$version"
 
 changeset tag
 
-git push --force --follow-tags origin "HEAD:refs/heads/${current_branch}"
+git push --force --follow-tags origin "HEAD:refs/heads/${branch_name}"

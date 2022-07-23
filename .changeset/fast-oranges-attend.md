8a1c6ee4f3ea6712282e7028ce7ea1fcbf6fe3f5
Jahred <jahredhope@gmail.com>
2022-07-23 22:35:55 +1000

    Add more important

---
+++ .changeset/fast-oranges-attend.md
@@ -0,0 +1,5 @@
+---
+"github-action-demo": patch
+---
+
+Exclamation more
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = getInput("who-to-greet");
   console.log(
-    `Hello ${nameToGreet}! Looks like you got this working. Congratulations.`
+    `Hello ${nameToGreet}! Looks like you got this working. Congratulations!`
   );
   const time = new Date().toTimeString();
   setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(context.payload, undefined, 2);
+
   console.log(`The event payload: ${payload}`);
 } catch (error: any) {
   setFailed(error.message);
